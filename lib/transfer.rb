479e4752486f75892a3ba9ca266e37f3b0a1a62a
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-31 04:04:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,7 +25,7 @@ end
      @status = "complete"
    else
      @status = "rejected"
-     return "Transaction rejected. Please check your account balance."
+     "Transaction rejected. Please check your account balance."
    end
  end
 end
