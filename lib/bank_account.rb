6739a3611be5b694523e3d1ba74fafba9538fe05
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-31 03:29:27 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,11 +1,12 @@
 class BankAccount
 
-    attr_accessor :balance, :status
+    attr_accessor :balance, :status, :deposit
     attr_reader :name
 
     def initialize(name)
       @name = name
       @balance = 1000
       @status = "open"
+      @deposit = depsosit
     end
 end
