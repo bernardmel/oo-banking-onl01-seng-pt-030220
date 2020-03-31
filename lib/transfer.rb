d8427f6d4af6e0acf8c5552fb06a2a9447cd82c1
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-31 04:10:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,8 +18,7 @@ end
   end
 
   def execute_transaction
-    if
-     @sender.balance > @amount && @status == "pending"
+    if @sender.balance > @amount && @status == "pending"
      @sender.balance -= @amount
      @receiver.balance += @amount
      @status = "complete"
